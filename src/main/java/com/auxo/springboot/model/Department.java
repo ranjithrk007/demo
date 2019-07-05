e92a7fb359b7d75a2075e652c6998cec97a87cc4
Ragu <ragu9921@gmail.com>
2019-07-05 15:27:07 +0530

    inital commit

---
+++ src/main/java/com/auxo/springboot/model/Department.java
@@ -0,0 +1,33 @@
+package com.auxo.springboot.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "department")
+@EntityListeners(AuditingEntityListener.class)
+public class Department {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id")
+	private long dprtid;
+	@Column(name = "name")
+	private String role;	
+	public long getDprtid() {
+		return dprtid;
+	}
+	public void setDprtid(long dprtid) {
+		this.dprtid = dprtid;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
